67c8eee9ff70e461e844d553f03bf49c46b3599c
Mojito-Fivem <66041893+Mojito-Fivem@users.noreply.github.com>
2020-09-14 16:43:39 +0100

    Update 1.1
    Added table for Gun Dealer Locations, these are taken from another script of mine - I do not recommend using these locations lol.

--- gd/config.lua
+++ gd/config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.Locale = "en"
 
-Config.DealerLocation = vector3(-589.56, -1618.72, 33.0)
+DealerLoc = vector3(-589.56, -1618.72, 33.0)
 
 Config.Dealer = "s_m_y_dealer_01"
 
@@ -14,3 +14,10 @@ Config.Weapons = {
 }
 
 Config.UseDirty = true
+
+Config.DealerLocations = {
+	[1] = {x = 23.62, y = -1751.11, z = 29.3,h = 86.0},
+	[2] = {x = -723.36, y = -913.11, z = 19.01,h = 86.0},
+	[3] = {x = 368.08, y = 340.61, z = 103.22,h = 86.0},
+	[4] = {x = -1507.6, y = -382.61, z = 40.93,h = 86.0},
+}
